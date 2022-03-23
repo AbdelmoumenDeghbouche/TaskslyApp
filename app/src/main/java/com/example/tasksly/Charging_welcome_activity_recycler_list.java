5dd0c5bfb26d0f8d451a90afc087a87a64c89b85
Abdelmoumen Deghbouche <a.deghbouche@esi-sba.dz>
2022-03-23 11:19:47 -0700

    I removed the main bleu background we need to backup to the ancient design i think it's better, we will let the new home action bar with changing of colors.

--- app/src/main/java/com/example/tasksly/Charging_welcome_activity_recycler_list.java
+++ app/src/main/java/com/example/tasksly/Charging_welcome_activity_recycler_list.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Charging_welcome_activity_recycler_list {
     public static ArrayList<welcom_activity_recycler_class> GetMainPageListe(){
-        ArrayList<welcom_activity_recycler_class> liste = new ArrayList<>();
-        liste.add(new welcom_activity_recycler_class(R.drawable.welcom2,"Welcome to Tasksly","Create an account to save all schedules\n and  access them from"));
-        liste.add(new welcom_activity_recycler_class(R.drawable.welcom1,"Organize your works","Let’s organize your works with priority and \n do everything without stress."));
-        return liste ;
+        ArrayList<welcom_activity_recycler_class> list = new ArrayList<>();
+        list.add(new welcom_activity_recycler_class(R.drawable.welcom2,"Welcome to Tasksly","Create an account to save all schedules\n and  access them from"));
+        list.add(new welcom_activity_recycler_class(R.drawable.welcom1,"Organize your works","Let’s organize your works with priority and \n do everything without stress."));
+        return list ;
     }
 
 }
