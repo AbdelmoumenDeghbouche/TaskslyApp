259d9f682aafef5e52e0f4dbd7dcd1690600d7ba
minouesi <86883036+minouesi@users.noreply.github.com>
2022-05-18 14:42:34 +0200

    ssss

--- app/src/main/java/com/example/tasksly/PayActivity.java
+++ app/src/main/java/com/example/tasksly/PayActivity.java
@@ -1,50 +1,19 @@
 package com.example.tasksly;
 
-import static android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS;
-
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowInsetsController;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 public class PayActivity extends AppCompatActivity {
-    RelativeLayout crypto_pay_option_button, google_pay_option_button;
-    ProgressBar progressBar ;
-    NetworkChangeListener networkChangeListener = new NetworkChangeListener();
-
+    Button crypto_pay_option_button, google_pay_option_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
-
-        getSupportActionBar().hide();
-
-
-        // changing the color of the status bar
-        this.getWindow().setStatusBarColor(this.getColor(R.color.white));
-
-        // to change the color of the icons in status bar to dark
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            this.getWindow().getDecorView().getWindowInsetsController().setSystemBarsAppearance(APPEARANCE_LIGHT_STATUS_BARS, APPEARANCE_LIGHT_STATUS_BARS);
-        }
-        // to change the color of the icons in the navigation bar to dark
-        getWindow().setNavigationBarColor(ContextCompat.getColor(PayActivity.this, R.color.white)); //setting bar color
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            this.getWindow().getDecorView().getWindowInsetsController().setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS, WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
-        }
-
-
-
-        progressBar = findViewById(R.id.progres);
         crypto_pay_option_button = findViewById(R.id.crypto_pay_option_button);
         google_pay_option_button = findViewById(R.id.google_pay_option_button);
         crypto_pay_option_button.setOnClickListener(new View.OnClickListener() {
@@ -52,25 +21,7 @@ public class PayActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(PayActivity.this, CryptoPay.class);
                 startActivity(intent);
-                progressBar.setVisibility(View.VISIBLE);
             }
         });
-
-    }
-    @Override
-    protected void onStart() {
-        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-        registerReceiver(networkChangeListener,intentFilter);
-        super.onStart();
-
-    }
-
-    @Override
-    protected void onStop() {
-
-        unregisterReceiver(networkChangeListener);
-        super.onStop();
-        progressBar.setVisibility(View.GONE);
-
     }
 }
